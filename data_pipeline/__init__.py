1429df416091e1d4ff833e08f69e8f8251885048
Alexander <alexbakker@live.nl>
2025-07-13 05:06:33 +0200

    Remove obsolete files: .gitignore, pyproject.toml, setscraper.py, uv.lock

--- data_pipeline/__init__.py
+++ data_pipeline/__init__.py
@@ -6,9 +6,12 @@ This package contains modules for scraping BBC 6 Music episodes and tracks.
 
 from .episodes import scrape_bbc6_episode, analyze_multiple_djs
 from .tracks import extract_tracks_from_episode
+from .episode_discovery import discover_episodes, discover_episodes_batch
 
 __all__ = [
     'scrape_bbc6_episode',
     'analyze_multiple_djs', 
-    'extract_tracks_from_episode'
+    'extract_tracks_from_episode',
+    'discover_episodes',
+    'discover_episodes_batch'
 ] 
